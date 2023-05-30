d8ef3adb229b7a6c6d9c0645a0156bf95243489e
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-05-29 19:00:27 -0500

    Complete the practice exercises for data types - string challenge

--- ruby_basics/1_data_types/exercises/string_exercises.rb
+++ ruby_basics/1_data_types/exercises/string_exercises.rb
@@ -5,36 +5,54 @@ end
 
 def concatenate(string)
   # use concatenation to format the result to be "Hello <string>!"
+  "Hello " << string
 end
+puts concatenate("Kaitlyn!")
 
 def substrings(word)
   # return the first 4 letters from the word using substrings
+  word[0, 4]
 end
+puts substrings("Supercalafragalistic")
 
 def capitalize(word)
   # capitalize the first letter of the word
+  word.capitalize
 end
+puts capitalize("animal")
 
 def uppercase(string)
   # uppercase all letters in the string
+  string.upcase
 end
+puts uppercase("ahhh")
 
 def downcase(string)
   # downcase all letters in the string
+  string.downcase
 end
+puts downcase("HEY YOU")
 
 def empty_string(string)
   # return true if the string is empty
+  string.empty?
 end
+puts empty_string("Aye yo")
 
 def string_length(string)
   # return the length of the string
+  string.length
 end
+puts string_length("Did you know that lightning travels up and not down?")
 
 def reverse(string)
   # return the same string, with all of its characters reversed
+  string.reverse
 end
+puts reverse("racecar")
 
 def space_remover(string)
   # remove all the spaces in the string using gsub
+  string.gsub(" ", "")
 end
+puts space_remover("In the vaccuum of space the human body will boil and freeze at the same time!")
